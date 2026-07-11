e2ad932b1b4e0c71c87ab1886da00979c35a20da
M02630129 <m1m02630129@oit.ac.jp>
2026-07-11 12:58:19 +0900

    add test for Calculate class

--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -4,4 +4,26 @@ public class Calculate {
   public int sum(int x, int y) {
     return x + y;
   }
+
+  public int getIndividualFee(int age) {
+    int fee = 0;
+
+    if (age <= 12) {
+      fee = 1000;
+    } else {
+      fee = 2000;
+    }
+
+    return fee;
+  }
+
+  public int getTotalFee(int age, int groupSize) {
+    int total = getIndividualFee(age) * groupSize;
+
+    if (groupSize >= 10) {
+      total = (int) (total * 0.9);
+    }
+
+    return total;
+  }
 }
--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -10,4 +10,39 @@ public class CalculateTest {
     int expected = 5;
     assertEquals(expected, calculate.sum(2, 3));
   }
+
+  @Test
+  public void testIndividualFeeForTen() {
+    Calculate calc = new Calculate();
+    int expected = 1000;
+    assertEquals(expected, calc.getIndividualFee(10));
+  }
+
+  @Test
+  public void testIndividualFee_Age12orLess_Returns1000() {
+    Calculate calc = new Calculate();
+    int expected = 1000;
+    assertEquals(expected, calc.getIndividualFee(12));
+  }
+
+  @Test
+  public void testIndividualFee_Age13orMore_Returns2000() {
+    Calculate calc = new Calculate();
+    int expected = 2000;
+    assertEquals(expected, calc.getIndividualFee(13));
+  }
+
+  @Test
+  public void testTotalFee_GroupSizeLessThan10_ReturnsNormalFee() {
+    Calculate calc = new Calculate();
+    int expected = 2000;
+    assertEquals(expected, calc.getTotalFee(13, 1));
+  }
+
+  @Test
+  public void testTotalFee_GroupSize10orMore_ReturnsDiscountedFee() {
+    Calculate calc = new Calculate();
+    int expected = 18000;
+    assertEquals(expected, calc.getTotalFee(13, 10));
+  }
 }
